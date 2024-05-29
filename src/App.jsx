adf93a840366774bac7c6ea4544a1c74d44e6119
Akilhan <oqiljondadaxanov@gmail.com>
2024-05-30 02:25:46 +0500

    SET UP FINISHED

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./componenets/Button";
+const App = () => {
   return (
     <>
-      <h1 className="underline text-red-700">Hello</h1>
+      <h1>Hello</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
     </>
   );
-}
+};
 
 export default App;
